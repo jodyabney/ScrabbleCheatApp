1f09203ab97f2caebe20acf6b5e9387e4a319998
Jody Abney <jodyabney@gmail.com>
2020-06-05 14:48:23 -0400

    Initial Commit

---
+++ ScrabbleCheatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrabbleCheatApp
+//
+//  Created by Jody Abney on 6/5/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
